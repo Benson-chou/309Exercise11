824e18600c6a98822a43e6478fc08d2935aa0e39
Benson-chou <benson.chou@mail.utoronto.ca>
2024-11-23 21:00:26 -0500

    fixed

--- E11/pages/api/csc309-e11.js
+++ E11/pages/api/csc309-e11.js
@@ -1,4 +1,4 @@
-export default handler = (req, res) => {
+export default function handler(req, res) {
   res.status(200).send(`<div id="auto-tester">
       Hi. I am chouch12, and I have learned how to develop a full-stack web app
       and deploy it!
